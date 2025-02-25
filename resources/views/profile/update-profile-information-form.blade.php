7c01007cce5d884b0abdcdedda5146d0c8bb109f
Afeefnih <afeefz2003@gmail.com>
2025-02-25 13:04:57 +0800

    Enhance dark mode support in form components; update styles for input fields and error messages, and improve layout in the profile update form.

--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -55,8 +55,8 @@
         @endif
 
         <!-- Name -->
-        <div class="col-span-6 sm:col-span-4">
-            <x-label for="name" value="{{ __('Name') }}"  />
+        <div class="col-span-6 sm:col-span-4 ">
+            <x-label for="name" value="{{ __('Name') }}" class="" />
             <x-input id="name" type="text" class="mt-1 block w-full dark:bg-gray-700 dark:text-white dark:border-gray-600" wire:model="state.name" required autocomplete="name" />
             <x-input-error for="name" class="mt-2" />
         </div>
